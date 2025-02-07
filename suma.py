f44ac8740c5f40f374e987c6cab0866524956ea6
Litzi Pineda <lpinedac7@miumg.edu.gt>
2025-02-06 23:00:32 -0600

    Subiendo programa de suma de dos numeros

---
+++ suma.py
@@ -0,0 +1,10 @@
+def main():
+    num1 = int(input("Ingrese el primer número: "))
+    num2 = int(input("Ingrese el segundo número: "))
+
+    suma = num1 + num2
+
+    print("El resultado es:", suma)
+
+if __name__ == "__main__":
+    main()
